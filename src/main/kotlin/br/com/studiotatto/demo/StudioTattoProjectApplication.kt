f4e203f74828881886c937153656b7d5745fd8dc
Erick Clinton <erickccleite@gmail.com>
2025-10-14 11:08:50 -0300

    init commit for project tatto

---
+++ src/main/kotlin/br/com/studiotatto/demo/StudioTattoProjectApplication.kt
@@ -0,0 +1,11 @@
+package br.com.studiotatto.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudioTattoProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<StudioTattoProjectApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/studiotatto/demo/StudioTattoProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.studiotatto.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudioTattoProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
