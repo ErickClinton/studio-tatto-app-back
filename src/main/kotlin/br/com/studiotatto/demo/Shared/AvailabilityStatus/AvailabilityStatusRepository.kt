18168fa80efb8e4efb224e8ab93c2db1a2523df1
Erick Clinton <erickccleite@gmail.com>
2025-10-15 13:50:38 -0300

    feat: creating repository

---
+++ src/main/kotlin/br/com/studiotatto/demo/Shared/AvailabilityStatus/AvailabilityStatusRepository.kt
@@ -0,0 +1,9 @@
+package br.com.studiotatto.demo.Shared.AvailabilityStatus
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.util.UUID
+
+@Repository
+interface AvailabilityStatusRepository : JpaRepository<AvailabilityStatusEntity, UUID> {
+}
